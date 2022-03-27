072ad92b7a19ffac3cb9076d9dd083a5f941f25a
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-03-27 22:15:10 +0300

    Redesigned the code

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import os
-from pprint import pprint
 
 import requests
 import telegram
 from dotenv import load_dotenv
 
 
-def main():
-    load_dotenv()
+def send_checking_result(telegram_token, chat_id, devman_response_json):
+    bot = telegram.Bot(token=telegram_token)
 
-    devman_token = os.getenv('DEVMAN_TOKEN')
-    telegram_token = os.getenv('TELEGRAM_TOKEN')
-    chat_id = os.getenv('CHAT_ID')
+    lesson_title = devman_response_json['new_attempts'][0]['lesson_title']
+    lesson_url = devman_response_json['new_attempts'][0]['lesson_url']
 
+    if not devman_response_json['new_attempts'][0]['is_negative']:
+        bot.send_message(
+            text=f'У вас проверили работу "{lesson_title}". \n\n'
+                 f'Преподователю все понравилось, можно приступать к следующему уроку!'
+                 f'Ссылка на урок: {lesson_url}',
+            chat_id=chat_id
+        )
+    else:
+        bot.send_message(
+            text=f'У вас проверили работу "{lesson_title}".\n\n'
+                 f'К сожалению в работе нашлись ошибки.\n\n'
+                 f'{lesson_url}',
+            chat_id=chat_id
+        )
+
+
+def check_devman_lesson_result(devman_token, telegram_token, chat_id):
     long_polling_url = 'https://dvmn.org/api/long_polling/'
     headers = {
         'Authorization': f'Token {devman_token}',
@@ -23,8 +38,8 @@ def main():
         try:
             response = requests.get(long_polling_url, params=params, headers=headers)
             response.raise_for_status()
-            dvmn_response_json = response.json()
-            if dvmn_response_json['status'] != 'timeout':
+            devman_response_json = response.json()
+            if devman_response_json['status'] != 'timeout':
                 params['timestamp'] = response.json()['new_attempts'][0]['timestamp']
             else:
                 continue
@@ -35,28 +50,26 @@ def main():
             print('Отсутствует подключение к интернету')
             continue
 
-        bot = telegram.Bot(token=telegram_token)
+        send_checking_result(
+            telegram_token=telegram_token,
+            chat_id=chat_id,
+            devman_response_json=devman_response_json
+        )
 
-        lesson_title = dvmn_response_json['new_attempts'][0]['lesson_title']
-        lesson_url = dvmn_response_json['new_attempts'][0]['lesson_url']
 
-        if not dvmn_response_json['new_attempts'][0]['is_negative']:
-            bot.send_message(
-                text=f'У вас проверили работу "{lesson_title}". \n\n'
-                     f'Преподователю все понравилось, можно приступать к следующему уроку!'
-                     f'Ссылка на урок: {lesson_url}',
-                chat_id=chat_id
-            )
-        else:
-            bot.send_message(
-                text=f'У вас проверили работу "{lesson_title}".\n\n'
-                     f'К сожалению в работе нашлись ошибки.\n\n'
-                     f'{lesson_url}',
-                chat_id=chat_id
-            )
 
-        pprint(response.json())
+def main():
+    load_dotenv()
 
+    devman_token = os.getenv('DEVMAN_TOKEN')
+    telegram_token = os.getenv('TELEGRAM_TOKEN')
+    chat_id = os.getenv('CHAT_ID')
+
+    check_devman_lesson_result(
+        devman_token=devman_token,
+        telegram_token=telegram_token,
+        chat_id=chat_id
+    )
 
 if __name__ == '__main__':
     main()
