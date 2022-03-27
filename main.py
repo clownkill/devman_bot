4da7f789492ecad12cf94ed1c24870f648d339a1
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-03-27 19:43:35 +0300

    added user data from long_poling devman api

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ load_dotenv()
 devman_token = os.getenv('DEVMAN_TOKEN')
 
 user_reviews_url = 'https://dvmn.org/api/user_reviews/'
+long_polling_url = 'https://dvmn.org/api/long_polling/'
 
 headers = {
     'Authorization': f'Token {devman_token}',
 }
 
-response = requests.get(user_reviews_url, headers=headers)
-response.raise_for_status()
+while True:
+    response = requests.get(long_polling_url, headers=headers)
+    response.raise_for_status()
 
-pprint(response.text)
+    pprint(response.json())
