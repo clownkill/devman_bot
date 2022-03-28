29a66d0aa167fb53332864c6bdcacbed0c623e78
clownkill <alehnovich90@yandex.ru>
2022-03-28 14:31:40 +0300

    add creating telegram bot instance

--- main.py
+++ main.py
@@ -5,21 +5,19 @@ import telegram
 from dotenv import load_dotenv
 
 
-def send_checking_result(telegram_token, telegram_chat_id, last_checking_attempt):
-    bot = telegram.Bot(token=telegram_token)
-
+def send_checking_result(telegram_bot, telegram_chat_id, last_checking_attempt):
     lesson_title = last_checking_attempt['lesson_title']
     lesson_url = last_checking_attempt['lesson_url']
 
     if not last_checking_attempt['is_negative']:
-        bot.send_message(
+        telegram_bot.send_message(
             text=f'У вас проверили работу "{lesson_title}". \n\n'
                  f'Преподователю все понравилось, можно приступать к следующему уроку!'
                  f'Ссылка на урок: {lesson_url}',
             chat_id=telegram_chat_id
         )
     else:
-        bot.send_message(
+        telegram_bot.send_message(
             text=f'У вас проверили работу "{lesson_title}".\n\n'
                  f'К сожалению в работе нашлись ошибки.\n\n'
                  f'{lesson_url}',
@@ -27,7 +25,7 @@ def send_checking_result(telegram_token, telegram_chat_id, last_checking_attempt
         )
 
 
-def check_devman_lesson_result(devman_token, telegram_token, telegram_chat_id):
+def check_devman_lesson_result(devman_token, telegram_bot, telegram_chat_id):
     long_polling_url = 'https://dvmn.org/api/long_polling/'
     headers = {
         'Authorization': f'Token {devman_token}',
@@ -52,7 +50,7 @@ def check_devman_lesson_result(devman_token, telegram_token, telegram_chat_id):
         params['timestamp'] = last_checking_attempt['timestamp']
 
         send_checking_result(
-            telegram_token=telegram_token,
+            telegram_bot=telegram_bot,
             telegram_chat_id=telegram_chat_id,
             last_checking_attempt=last_checking_attempt
         )
@@ -65,9 +63,11 @@ def main():
     telegram_token = os.getenv('TELEGRAM_TOKEN')
     telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
 
+    bot = telegram.Bot(telegram_token)
+
     check_devman_lesson_result(
         devman_token=devman_token,
-        telegram_token=telegram_token,
+        telegram_bot = bot,
         telegram_chat_id=telegram_chat_id
     )
 
