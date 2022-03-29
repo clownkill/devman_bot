450cbb8a02f901da7adde3bb273f750dbcde4158
clownkill <alehnovich90@yandex.ru>
2022-03-29 10:23:19 +0300

    reformat logging message

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    format="%(asctime)s : %(name)s : %(levelname)s : %(message)s",
     level=logging.INFO
 )
 logging.getLogger(__name__)
